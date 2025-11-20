ae049fc9d04bddbf9f0cc9b1261631eb6063318e
kovalevm2207 <kovalevm220741@gmail.com>
2025-11-20 22:26:48 +0300

    Написал функцию чтения узла из файла

--- I_Love_Math.cpp
+++ I_Love_Math.cpp
@@ -14,11 +14,43 @@ int main()
     return 0;
 }
 
-
+// после себя всегда оставляет указатель на не пробельный символ
 Node_t* GetTreeNode(char** cur_pos)
 {
     assert( cur_pos && "NULL ptr on buffer ptr, check GetTreeNode func");
     assert(*cur_pos && "NULL ptr on buffer, check SkipSpaces func");
 
-    return NULL;
+    typedef struct
+    {
+        NodeType_t type;
+        Value_t value;
+    } data_t;
+
+    if(**cur_pos == '(')                              // мы могли встретить новый узел
+    {                                                 // тогда:
+        *cur_pos = SkipSpaces(++*cur_pos);            // пропустили скобку, потом пробелы после скобки
+                                                      // теперь мы на первом символе слова
+        data_t word = GetAndUnuliseWord(*cur_pos);    // считываем это слово и анализируем, что нам попалось
+        Node_t* node = TreeNodeCtor(word, GetTreeNode(cur_pos), GetTreeNode(cur_pos)); // создаем узел с нужным значнием и прикрепляем к нему поддеревья
+        if (!node) return NULL;                       // не забываем сделать проверку, на то, что узел успешно создался
+        // т.к. GetTreeNode после себя оставляет указатель на непробельный символ, то мы должны встретить ')' после прочтения поддеревьев
+        if (*cur_pos == ')')
+        {  // все хорошо, узел считался правильно
+            *cur_pos = SkipSpaces(++*cur_pos);        // пропускаем скобку и пробельные символы послее нее
+            return node;                              // возвращаем указатель на получившийся узел
+        }
+        else
+        {  // все плохо, узел считался неправильно
+            FREE(node);                               // очищаем память, если надо
+            *cur_pos = SkipSpaces(++*cur_pos);        // пропускаем скобку и пробельные символы послее нее
+            return NULL;
+        }                                             // чтение узла закончили
+    }
+    else if(strncmp(*cur_pos, "nil") == 0)            // но также мы могли встретить еще и "nil"
+    {                                                 // тогда:
+        *cur_pos += sizeof("nil") - 1;                // прпускаем "nil"
+        *cur_pos = SkipSpaces(*cur_pos);              // пропускаем все пробельные символы
+        return NULL;
+    }
+    else return NULL;                                 // во всех остальных случаях возвращаем NULL, не смещаясь с некорректного символа
 }
