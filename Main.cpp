f8ce7188646f2dbf2459c817730aa2f7a8c2d1dd
kovalevm2207 <kovalevm220741@gmail.com>
2025-12-17 22:38:23 +0300

    Main be like, not compiling

--- Main.cpp
+++ Main.cpp
@@ -1,26 +1,31 @@
 #include "Math_PP.h"
 
-int main()
+int main(const int argc, const char* const argv[])
 {
     int count_img = 0;
+    const char* input_file = NULL;
     srand((unsigned int) time(NULL));
     StartHTMLfile();
-
-    Tree_t* user_tree = GetExpression("Expression.txt");
+    if(argc != 2)
+    {
+        HelpUser();
+        input_file = "Expression.txt";
+    }else input_file = argv[1];
+    ProgramData_t input_data = GetInputData(input_file);
 
     FILE* tex_file = fopen("LaTeX.tex","w");
     assert(tex_file);
     BeginLaTeXDocument(tex_file);
-    PrintOriginalTree(tex_file, user_tree->root);
+    PrintOriginalTree(tex_file, input_data.user_tree->root);
 
-    Derivative_t* derivatives = GetNDerivatives(tex_file, user_tree, &count_img);
+    Derivative_t* derivatives = GetNDerivatives(tex_file, input_data, &count_img);
     Tree_t* taylor_tree = TaylorExpansion(tex_file, derivatives);
 
-    EndProgram(tex_file, derivatives);
+    EndProgram(tex_file, derivatives, input_data);
     TreeDtor(&taylor_tree);
     return 0;
 }
-void EndProgram(FILE* tex_file, Derivative_t* derivatives)
+void EndProgram(FILE* tex_file, Derivative_t* derivatives, ProgramData_t input_data)
 {
     assert(tex_file);
     assert(derivatives);
@@ -30,6 +35,13 @@ void EndProgram(FILE* tex_file, Derivative_t* derivatives)
     tex_file = NULL;
 
     EndHTMLfile();
+
+    input_data.user_tree = NULL;
+    input_data.taylor_order = 0;
+    input_data.x.left_border = 0;
+    input_data.x.right_border = 0;
+    input_data.y.left_border = 0;
+    input_data.y.right_border = 0;
 
     for(int i = 0; i <= TAYLOR_ORDER; i++)
     {
